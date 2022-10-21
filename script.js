f28aaa6f1692309bed9e53d5bd0c26230dc1c283
Fabian Caro <fabiancaro101@gmail.com>
2022-10-21 12:47:03 -0500

    Escribiendo HTML desde JavaScript

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 const h1 = document.querySelector('h1');
 const p = document.querySelector('p');
 const parrafito = document.getElementsByClassName('.parrafito');
-const pid = document.getElementById('#pid');
+const pid = document.getElementById('pid');
 const input = document.querySelector('input');
 
 console.log(input.value);
@@ -17,4 +17,25 @@ console.log({
     pid,
     input,
 });
+
+
+h1.innerHTML = 'Patito <br> Feo';
+h1.innerText = 'Patito <br> Feo';
+//console.log(h1.getAttribute('pantalla'));
+//h1.setAttribute('class', 'rojo');
+
+h1.classList.add('rojo');
+h1.classList.remove('verde');
+// h1.classList.toggle('verde');
+// h1.classList.contains('verde');
+
+input.value = "456";
+
+const img = document.createElement('img')
+img.setAttribute('src', 'https://tresubresdobles.com/wp-content/uploads/2021/04/skft-23aff38e10ee3c4e430a1f3450c4a01d.jpeg');
+console.log(img);
+
+pid.innerHTML = "";
+pid.append(img);
+
 
